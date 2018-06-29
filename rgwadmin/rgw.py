f94199b6f921114c82396660089678cb6de5c41b
Jacek Jablonski <jacek.jablonski@allegro.pl>
2018-06-29 21:20:44 +0200

    add stats for get_user

--- rgwadmin/rgw.py
+++ rgwadmin/rgw.py
@@ -165,9 +165,10 @@ class RGWAdmin:
         else:
             return None
 
-    def get_user(self, uid):
-        return self.request('get', '/%s/user?format=%s&uid=%s' %
-                            (self._admin, self._response, uid))
+    def get_user(self, uid, stats=False):
+        parameters = '&uid=%s&stats=%s' % (uid, stats)
+        return self.request('get', '/%s/user?format=%s%s'
+                            (self._admin, self._response, parameters))
 
     def get_users(self):
         return self.get_metadata(metadata_type='user')
